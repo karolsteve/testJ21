71927ac0aaf2ebb0e6c34ec2f717ac634a9d66a2
Steve Tchatchouang <steve.tchatchouang2015@gmail.com>
2024-02-01 14:06:49 +0100

    Initial commit

---
+++ src/main/java/com/ondjoss/Main.java
@@ -0,0 +1,18 @@
+package com.ondjoss;
+
+/**
+ * Created by Steve Tchatchouang at 30/12/2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        int age = 10;
+        switch (age) {
+            case 1:
+                System.out.println("YES");
+                break;
+            default:
+                System.out.println("TARAA");
+                break;
+        }
+    }
+}
